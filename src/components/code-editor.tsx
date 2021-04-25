63316728f22ab8ba97486ad59a8c224ea7dbdef3
zakiafada32 <afadazaki32@gmail.com>
2021-04-25 22:38:03 +0700

    add prettier formatter

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react';
 import MonacoEditor, { OnMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -6,31 +9,54 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>(); // todo update type
+
   const onEditorDidMount: OnMount = (monacoEditor) => {
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       onChange(monacoEditor.getValue());
     });
   };
 
+  const onFormatClick = () => {
+    // get current value from editor
+    const unformatted = editorRef.current.getValue();
+
+    // format that value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+
+    // set the formatted value back in the editor
+    editorRef.current.setValue(formatted);
+  };
+
   return (
-    <MonacoEditor
-      onMount={onEditorDidMount}
-      defaultValue={initialValue}
-      theme="vs-dark"
-      language="javascript"
-      height="500px"
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-        tabSize: 2,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        onMount={onEditorDidMount}
+        defaultValue={initialValue}
+        theme="vs-dark"
+        language="javascript"
+        height="500px"
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2,
+        }}
+      />
+    </div>
   );
 };
 

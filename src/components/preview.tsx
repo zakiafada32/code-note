ce21a7f8f9025066943ee187477086c814a2bb33
zakiafada32 <afadazaki32@gmail.com>
2021-04-26 12:21:44 +0700

    button for reset iframe

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react';
 
+import './code-editor.css';
 interface PreviewProps {
   code: string;
 }
@@ -28,23 +29,35 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
-    const setIframe = async () => {
-      if (iframe.current === null) {
-        return;
-      }
-      // iframe.current.srcdoc = html; // todo research iframe reloading
-      iframe.current.contentWindow?.postMessage(code, '*');
-    };
-    setIframe();
+    if (iframe.current === null) {
+      return;
+    }
+
+    iframe.current.contentWindow?.postMessage(code, '*');
   }, [code]);
 
+  const onResetClick = () => {
+    if (iframe.current === null) {
+      return;
+    }
+    iframe.current.srcdoc = html; // todo research iframe reloading
+  };
+
   return (
-    <iframe
-      ref={iframe}
-      srcDoc={html}
-      sandbox="allow-scripts"
-      title="sandbox frame"
-    />
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onResetClick}
+      >
+        Format
+      </button>
+      <iframe
+        ref={iframe}
+        srcDoc={html}
+        sandbox="allow-scripts"
+        title="sandbox frame"
+      />
+    </div>
   );
 };
 

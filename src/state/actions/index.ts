839c8eef633055f45d6abc8b7583274b34dbef21
zakiafada32 <afadazaki32@gmail.com>
2021-05-02 22:52:31 +0700

    update reducer

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -12,13 +12,15 @@ export interface MoveCellAction {
 
 export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
-  payload: string;
+  payload: {
+    id: string;
+  };
 }
 
 export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
   payload: {
-    id: string;
+    id: string | null;
     type: CellTypes;
   };
 }
